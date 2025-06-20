a81092a40c2ae329ae2a87118705dbaeea24f5d2
anthoni portocarrero rodriguez <softwaretoni21@gmail.com>
2025-06-20 00:15:04 -0500

    init

---
+++ src/main.ts
@@ -0,0 +1,38 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { AppModule } from './app.module';
+import { envs } from './config/envs';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.NATS,
+      options: {
+        servers: [envs.NATS_SERVERS],
+      },
+    },
+  );
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  await app.listen();
+  console.log(
+    `🚀 Microservice Point running with NATS on ${envs.NATS_SERVERS}`,
+  );
+}
+
+bootstrap().catch((err) => {
+  console.error('💥 Error fatal durante el bootstrap:', err);
+  process.exit(1);
+});
