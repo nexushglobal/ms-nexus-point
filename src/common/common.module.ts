e0ee7f0e7461a381f073ed779bdd83fd1237010d
JosmenDev <josmendev@gmail.com>
2025-09-09 23:41:29 -0500

    feat(api): initial configuration

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './services/users.service';
 import { MembershipService } from './services/memberships.service';
 import { OrdersService } from './services/orders.service';
+import { PaymentsService } from './services/payments.service';
 
 @Module({
-  providers: [UsersService, MembershipService, OrdersService],
-  exports: [UsersService, MembershipService, OrdersService],
+  providers: [UsersService, MembershipService, OrdersService, PaymentsService],
+  exports: [UsersService, MembershipService, OrdersService, PaymentsService],
 })
 export class CommonModule {}
